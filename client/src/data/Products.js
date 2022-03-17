a53906389f012b4f5eed1d62ca2cecb3a3e38f11
Raghu-vr <raghuveer.b.rv@gmail.com>
2022-03-17 11:25:14 +0530

    Initial commit

---
+++ client/src/data/Products.js
@@ -0,0 +1,70 @@
+const products = [
+  {
+    _id: "1",
+    name: "Sony Gold-Plated Wireless Controller",
+    image: "/images/6.png",
+    description:
+      "Heighten Your Senses™",
+    price: 799,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    _id: "2",
+    name: "Sony DualSense™ Wireless Controller",
+    image: "/images/5.png",
+    description:
+      "Discover a deeper gaming experience1 with the innovative new PS5™ controller.",
+    price: 99,
+    countInStock: 10,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    _id: "3",
+    name: "Oculus Quest 2",
+    image: "/images/4.png",
+    description:
+      "AN ALL-IN-ONE HEADSET FILLED WITH HUNDREDS OF UNIQUE EXPERIENCES",
+    price: 429,
+    countInStock: 2,
+    rating: 3.5,
+    numReviews: 3,
+  },
+  {
+    _id: "4",
+    name: "Nintendo Switch - Neon blue Neon Red Joy-con",
+    image: "/images/3.png",
+    description:
+      "Get The Gaming System That Lets You Play The Games You Want, Wherever You Are, However You Like!",
+    price: 299,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 9,
+  },
+  {
+    _id: "5",
+    name: "Microsoft Xbox Series X Console",
+    image: "/images/2.png",
+    description:
+      "The fastest and most powerful Xbox ever",
+    price: 490,
+    countInStock: 7,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    _id: "6",
+    name: "Sony Playstation PS5 Console",
+    image: "/images/1.png",
+    description:
+      "Play PS5 and PS4 games on Blu-ray discs. You can also download digital PS5 and PS4 games from PlayStation® Store.",
+    price: 500,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 50,
+  },
+];
+
+export default products;
